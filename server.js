2e1dc6ec6895b54762fd9ec5dca679b544e0e57e
RachelY <yang.yijing@outlook.com>
2023-12-16 19:30:49 -0500

    Tip calculator express application created.

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.get("/greeting/:name",(req,res) => {
     res.send(`<h1>${name}! It's very nice meeting you.</h1>`)
 })
 
+app.get('/tip/:total/:percentage', (req,res) => {
+    const total = req.params.total
+    const percentage = req.params.percentage
+    const tipAmount = total * (percentage/100)
+    res.send(`${tipAmount}`)//Don't try to put the number in res.send() always do the interpolation
+})
+
 
 // app.listen(PORT, FUNCTION)
 // The function will run after the server turns on
-app.listen(3000, () => {console.log("server is listening on port 3000")})
+app.listen(3001, () => {console.log("server is listening on port 3001")})
