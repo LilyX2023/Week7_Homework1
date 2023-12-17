e7b831f7311749aecb8d03f6548d3466cdef0f34
RachelY <yang.yijing@outlook.com>
2023-12-16 19:54:09 -0500

    Magic 8 ball express application created

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.get('/tip/:total/:percentage', (req,res) => {
     res.send(`${tipAmount}`)//Don't try to put the number in res.send() always do the interpolation
 })
 
+app.get('/magic/:question', (req, res) => {
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    const randomResponses = responses[Math.floor(Math.random() * responses.length)]
+    res.send(`<h1>${randomResponses}<h1>`)
+})
+
 
 // app.listen(PORT, FUNCTION)
 // The function will run after the server turns on
